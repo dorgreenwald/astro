b3adc17c500fe90b1abcd38d0a7f1bb29cb8a50b
dorgreenwald <dor.greenwald@gmail.com>
2020-11-12 20:31:24 +0200

    initial version

---
+++ App_Code/DBFunctions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for DBFunctions
+/// </summary>
+public class DBFunctions
+{
+    public static string connStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\dorgr\source\repos\astro\astro\App_Data\Database.mdf;Integrated Security=True";
+}
