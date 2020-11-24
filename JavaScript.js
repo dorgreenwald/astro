03e0746d240a5b99ca14b1cf23f2aac07edf4830
dorgreenwald <dor.greenwald@gmail.com>
2020-11-24 23:38:46 +0200

    not working

---
+++ JavaScript.js
@@ -0,0 +1,12 @@
+﻿function hidden_galaxy() {
+    document.getElementById(more-info).style.display = inherit;
+}
+function hidden_nebula() {
+    document.getElementById(more-info).style.display = inherit;
+}
+function hidden_blackHole() {
+    document.getElementById(more-info).style.display = inherit;
+}
+function hidden_solarSystem() {
+    document.getElementById(more-info).style.display = inherit;
+}
