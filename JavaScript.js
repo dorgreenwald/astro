ddf4c849adc86b05a47dd9d9d0583d75ad290f23
dorgreenwald <dor.greenwald@gmail.com>
2020-11-27 17:57:39 +0200

    add more info window + more pictures

--- JavaScript.js
+++ JavaScript.js
@@ -1,12 +1,34 @@
 ﻿function hidden_galaxy() {
-    document.getElementById(more-info).style.display = inherit;
+    document.getElementById("more_info_galaxys").style.display = "inherit";
+    document.getElementById("more_info_nebulas").style.display = "none";
+    document.getElementById("more_info_blackHoles").style.display = "none";
+    document.getElementById("more_info_solarSystem").style.display = "none";
 }
 function hidden_nebula() {
-    document.getElementById(more-info).style.display = inherit;
+    document.getElementById("more_info_galaxys").style.display = "none";
+    document.getElementById("more_info_nebulas").style.display = "inherit";
+    document.getElementById("more_info_blackHoles").style.display = "none";
+    document.getElementById("more_info_solarSystem").style.display = "none";
 }
 function hidden_blackHole() {
-    document.getElementById(more-info).style.display = inherit;
+    document.getElementById("more_info_galaxys").style.display = "none";
+    document.getElementById("more_info_nebulas").style.display = "none";
+    document.getElementById("more_info_blackHoles").style.display = "inherit";
+    document.getElementById("more_info_solarSystem").style.display = "none";
 }
 function hidden_solarSystem() {
-    document.getElementById(more-info).style.display = inherit;
+    document.getElementById("more_info_galaxys").style.display = "none";
+    document.getElementById("more_info_nebulas").style.display = "none";
+    document.getElementById("more_info_blackHoles").style.display = "none";
+    document.getElementById("more_info_solarSystem").style.display = "inherit";
+}
+function exit() {
+    document.getElementById("more_info_galaxys").style.display = "none";
+    document.getElementById("more_info_nebulas").style.display = "none";
+    document.getElementById("more_info_blackHoles").style.display = "none";
+    document.getElementById("more_info_solarSystem").style.display = "none";
+}
+function more_pics() {
+    document.getElementById("more_pics").style.display = "inherit";
+    document.getElementById("btn_more_pics").style.display= "none";
 }
