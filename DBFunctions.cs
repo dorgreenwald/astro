2063590d720149eabb1fe8018d20edaaea009e52
dorgreenwald <dor.greenwald@gmail.com>
2020-12-07 20:13:03 +0200

    things

--- DBFunctions.cs
+++ DBFunctions.cs
@@ -10,4 +10,12 @@ using System.Data.SqlClient;
 public class DBFunctions
 {
     public static string connStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\dorgr\source\repos\astro\astro\App_Data\Database.mdf;Integrated Security=True";
+public static void RunNonQuery(string query)
+    {
+        SqlConnection conn = new SqlConnection(connStr);
+        SqlCommand cmd = new SqlCommand(query, conn);
+        conn.Open();
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
 }
--- JavaScript.js
+++ JavaScript.js
@@ -3,38 +3,52 @@
     document.getElementById("more_info_nebulas").style.display = "none";
     document.getElementById("more_info_blackHoles").style.display = "none";
     document.getElementById("more_info_solarSystem").style.display = "none";
+    document.getElementById("overlay").style.display = "inherit";
 }
 function hidden_nebula() {
     document.getElementById("more_info_galaxys").style.display = "none";
     document.getElementById("more_info_nebulas").style.display = "inherit";
     document.getElementById("more_info_blackHoles").style.display = "none";
     document.getElementById("more_info_solarSystem").style.display = "none";
+    document.getElementById("overlay").style.display = "inherit";
 }
 function hidden_blackHole() {
     document.getElementById("more_info_galaxys").style.display = "none";
     document.getElementById("more_info_nebulas").style.display = "none";
     document.getElementById("more_info_blackHoles").style.display = "inherit";
     document.getElementById("more_info_solarSystem").style.display = "none";
+    document.getElementById("overlay").style.display = "inherit";
 }
 function hidden_solarSystem() {
     document.getElementById("more_info_galaxys").style.display = "none";
     document.getElementById("more_info_nebulas").style.display = "none";
     document.getElementById("more_info_blackHoles").style.display = "none";
     document.getElementById("more_info_solarSystem").style.display = "inherit";
+    document.getElementById("overlay").style.display = "inherit";
+
 }
 function exit() {
     document.getElementById("more_info_galaxys").style.display = "none";
     document.getElementById("more_info_nebulas").style.display = "none";
     document.getElementById("more_info_blackHoles").style.display = "none";
     document.getElementById("more_info_solarSystem").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
 }
-function more_pics() {
-    document.getElementById("more_pics").style.display = "inherit";
-    document.getElementById("btn_more_pics").style.display= "none";
-    document.getElementById("btn_less_pics").style.display= "inherit";
+function more_or_less_pics() {
+    if (document.getElementById("btn_more_or_less_pics").innerText == "more >")
+    {
+        document.getElementById("more_pics").style.display = "inherit";
+        document.getElementById("btn_more_or_less_pics").innerText = " < less";
+    }
+    else
+    {
+        document.getElementById("more_pics").style.display = "none";
+        document.getElementById("btn_more_or_less_pics").innerText = "more >";
+    }
 }
-function less_pics() {
-    document.getElementById("more_pics").style.display = "none";
-    document.getElementById("btn_more_pics").style.display= "inherit";
-    document.getElementById("btn_less_pics").style.display= "none";
+function show_text_galaxy() {
+    document.getElementById("text_galaxy").style.display = "inherit";
+}
+function less_text_galaxy() {
+    document.getElementById("text_galaxy").style.display = "none";
 }
