bf04b855e73d782121b309772f6ad930ff83151c
dorgreenwald <dor.greenwald@gmail.com>
2021-05-08 10:31:07 +0300

    quiz (not working)

--- quiz1.js
+++ quiz1.js
@@ -1,13 +1,4 @@
-﻿function level1() {
-    document.getElementById("level1").style.display = "inherit";
-    document.getElementById("level2").style.display = "none";
-    document.getElementById("level3").style.display = "none";
-    document.getElementById("level4").style.display = "none";
-    document.getElementById("level5").style.display = "none";
-    document.getElementById("choose-level").style.display = "none";
-    document.getElementById("next-level").style.display = "inherit";
-    document.getElementById("previous-level").style.display = "inherit";
-}
+
 function level2() {
     document.getElementById("level1").style.display = "none";
     document.getElementById("level2").style.display = "inherit";
@@ -35,27 +26,33 @@ function next_level() {
 function previous_level() {
     document.getElementById("previous-level").innerText = "Previous level >";
 }
-function question1() {
-    if (document.getElementById("next1").innerText == "Check answer") {
-        if (document.getElementById("ques1").innerText == 8) {
-            document.getElementById("next1").innerText = "Next question >";
-            document.getElementById("quesbox1").style.borderColor = "green";
-        }
-        else {
-            document.getElementById("quesbox1").style.borderColor = "red";
-            document.getElementById("next1").innerText = "Next question >";
-
-        }
-    }
-    else
-    {
-        document.getElementById("quesbox1").style.display = "none";
-        document.getElementById("quesbox2").style.display = "inherit";
-    }
+function question1()
+{
+    alert("hi")
 }
+// function question1() {
+//     alert("document.getElementById.nodeValue");
+//     var answer = document.getElementById("ques1").value;
+//     if (document.getElementById("next1").nodeValue == "Check answer") {
+//         if ((answer == "8")||(answer == 8)) {
+//             document.getElementById("next1").innerText = "Next question >";
+//             document.getElementById("quesbox1").style.borderColor = "green";
+//         }
+//         else {
+//             document.getElementById("next1").innerText = "Next question >";
+//             document.getElementById("quesbox1").style.borderColor = "red";
+//         }
+//     }
+//     if (document.getElementById("next1").innerText == "Next question >") {
+//         {
+//             document.getElementById("quesbox1").style.display = "none";
+//             document.getElementById("quesbox2").style.display = "inherit";
+//         }
+//     }
+// }
 function question2() {
-    if (document.getElementById("next2").innerText == "Check answer") {
-        if ((document.getElementById("ques2").innerText == "venus") || (document.getElementById("ques2").innerText == "Venus")) {
+    if (document.getElementById("next2").nodeValue == "Check answer") {
+        if ((document.getElementById("ques2").nodeValue == "venus") || (document.getElementById("ques2").innerText == "Venus")) {
             document.getElementById("next2").innerText = "Next question >";
             document.getElementById("quesbox2").style.borderColor = "green";
         }
